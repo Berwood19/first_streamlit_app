38817c8edeb39c8d00e6aafdab4e14f4eb0e2c3f
Berwood19 <126572327+Berwood19@users.noreply.github.com>
2023-05-10 14:30:19 +0100

    Update streamlit_app.py
    Added some error trapping logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,20 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display Fruityvice api response 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
-# normalise the json to break into columns 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output the normalised data in a table
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  #streamlit.write('The user entered ', fruit_choice)
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # normalise the json to break into columns 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output the normalised data in a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error
+  
 # don't run past here
 streamlit.stop()
 
